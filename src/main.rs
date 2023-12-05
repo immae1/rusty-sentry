da3f58152e1eb543ebce248f853977e2ce5f4977
Immanuel Haag <9929800+immae1@users.noreply.github.com>
2023-12-05 09:16:22 +0100

    loop and send some sentry events

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-fn main() {
+ fn main() {
     let url = env::var("SENTRY_URL").unwrap();
     let _guard: sentry::ClientInitGuard = sentry::init((url, sentry::ClientOptions {
     release: sentry::release_name!(),
@@ -8,5 +8,30 @@ fn main() {
     }));
 
      // Sentry will capture this
-    panic!("Everything is on fire!");
+
+     let mut count = 0u32;
+
+     loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            // Skip the rest of this iteration
+            continue;
+        }
+
+        println!("{}", count);  
+        sentry::capture_message("Something went wrong", sentry::Level::Info);
+
+        if count == 50000 {
+            println!("OK, that's enough");
+            panic!("Party party party - Everything is on fire!");
+
+            // Exit this loop
+            //break;
+        }
+
+    }
 }
+ 
